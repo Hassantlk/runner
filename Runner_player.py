98431913f2fa6f53704bcd88be653d3f11b0f535
Hassan.TLK <hassan.tlk.85@gmail.com>
2023-09-09 18:17:05 +0330

    add instance of runner

--- Runner_player.py
+++ Runner_player.py
@@ -2,10 +2,10 @@ class Runner:
     def __init__(self, name, speed):
         self.name = name
         self.speed = speed
-    
-    
+
     def __str__(self):
         return f"{self.name}___{self.speed}"
+
 
 with open("runner_name.txt") as f:
     text = f.readlines()
@@ -19,3 +19,9 @@ with open("runner_name.txt") as f:
 
 # print(person)
 
+rnr_john_10 = Runner("john", 10)
+rnr_jack_8 = Runner("jack", 8)
+rnr_james_6 = Runner("james", 6)
+rnr_oliver_9 = Runner("oliver", 9)
+
+print(rnr_james_6)
