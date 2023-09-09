874568ed1330c27cd565d409cd4b987206d7835f
Hassan.TLK <hassan.tlk.85@gmail.com>
2023-09-09 18:31:42 +0330

    add step method

--- Runner_player.py
+++ Runner_player.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Runner:
     def __init__(self, name, speed):
         self.name = name
@@ -5,6 +8,11 @@ class Runner:
 
     def __str__(self):
         return f"{self.name}___{self.speed}"
+
+    def step(self):
+        rnd = random.choice([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1])
+        return rnd*self.speed
+
 
 
 with open("runner_name.txt") as f:
