727af367faa4266095e275243192973cf62b961c
Hassan.TLK <hassan.tlk.85@gmail.com>
2023-09-11 20:40:24 +0330

    improve loging

--- Runner_player.py
+++ Runner_player.py
@@ -12,20 +12,11 @@ class Runner:
 
     def step(self):
         rnd = random.uniform(0, 1)
-        # rnd = random.choice([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1])
         self.location += rnd*self.speed
 
     def get_location(self):
         return self.location
 
-
-# runners = {}
-# rnr_john_10 = Runner("john", 10)
-# rnr_jack_8 = Runner("jack", 8)
-# rnr_james_6 = Runner("james", 6)
-# rnr_oliver_9 = Runner("oliver", 9)
-
-# rnr_list = [rnr_john_10,rnr_jack_8,rnr_james_6,rnr_oliver_9]
 runners = []
 
 with open('runner_name.txt') as file:
@@ -34,29 +25,34 @@ with open('runner_name.txt') as file:
         name, speed = line.strip().split(' ')
         runners.append(Runner(name, float(speed)))
 
-
 end_line = 10
 winner = None
 
-
-
-while True:
-    for runner in runners:
-        runner.step()
-        location = runner.get_location()
-        print(f"{runner.name}: {'-' * int(location)}")
-        time.sleep(0.5)
-        
-        if location >= end_line:
-            winner = runner
-            break
-    
-    if winner:
-        break
-
-
 with open('runner.log', 'w') as file:
-    for runner in runners:
-        file.write(f"{runner.name}: {runner.get_location()}\n")
+    file.write("Starting \n")
+
+with open('runner.log', 'a') as file:
+    step=0
+    while True:
+        for runner in runners:
+            runner.step()
+            location = runner.get_location()
+            print(f"{runner.name}: {'-' * int(location)}")
+            file.write(f"[{step}: {runner.name}]: {runner.get_location()}\n") 
+            time.sleep(0.5)
+
+            
+            if location >= end_line:
+                winner = runner
+                break
+        step +=1
+                    
+
+
+        if winner:
+            break
+
+
+
 
 print(f"The winner is {winner.name}!")
